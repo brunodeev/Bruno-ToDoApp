b79bc1503fc69ecfcef349f85add7bd0beafda41
Bruno Meireles <bcgmeireles@gmail.com>
2025-06-06 12:25:47 -0300

    feat: creates a hibernate config file

--- src/main/java/com/bruno/database/DbInitializer.java
+++ src/main/java/com/bruno/database/DbInitializer.java
@@ -4,31 +4,18 @@ import java.sql.*;
 
 public class DbInitializer {
     public static void createDatabases() {
-        String dbUrl = "jdbc:h2:mem:todo";
-        String dbUser = "sa";
-        String dbPassword = "";
-
         String sql = """
             CREATE TABLE IF NOT EXISTS tasks (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 name VARCHAR(255) NOT NULL,
                 completed BOOLEAN NOT NULL
             );
-        
-            CREATE TABLE IF NOT EXISTS users (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL
-            );
         """;
 
-        try {
-            Connection conn = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        try (Connection conn = DbConnection.connect()){
             Statement statement = conn.createStatement();
 
             statement.execute(sql);
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
