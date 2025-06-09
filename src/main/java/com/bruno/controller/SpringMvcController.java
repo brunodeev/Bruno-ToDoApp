d8b1268603c0e6b658706ece4b1a65ee5274bcf4
Bruno Meireles <bcgmeireles@gmail.com>
2025-06-09 15:26:19 -0300

    feat: implements add new task endpoints

--- src/main/java/com/bruno/controller/SpringMvcController.java
+++ src/main/java/com/bruno/controller/SpringMvcController.java
@@ -3,9 +3,12 @@ package com.bruno.controller;
 import com.bruno.dao.TaskDao;
 import com.bruno.factory.BeanFactory;
 import com.bruno.model.Task;
+import com.bruno.model.TaskHibernate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -19,6 +22,20 @@ public class SpringMvcController {
         List<Task> tasks = taskDao.listAllTasks();
         model.addAttribute("tasks", tasks);
 
-        return "hello";
+        return "list";
+    }
+
+    @GetMapping("/create")
+    public String createPage(Model model) {
+        model.addAttribute("task", new TaskHibernate());
+
+        return "create";
+    }
+
+    @PostMapping("/create")
+    public String createTask(@ModelAttribute TaskHibernate task) {
+        taskDao.addTask(task);
+
+        return "redirect:/list";
     }
 }
--- src/main/java/com/bruno/model/TaskHibernate.java
+++ src/main/java/com/bruno/model/TaskHibernate.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tasks")
-public class TaskHibernate implements Task{
+public class TaskHibernate implements Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
