f18fc3bc88ce76e40bbc86a796ac403e7644dacd
Bruno Meireles <bcgmeireles@gmail.com>
2025-06-26 16:11:45 -0300

    test: implements list tasks page with task test case

--- src/test/java/com/bruno/wicket/ListTasksPageTest.java
+++ src/test/java/com/bruno/wicket/ListTasksPageTest.java
@@ -2,11 +2,14 @@ package com.bruno.wicket;
 
 import com.bruno.application.ApplicationTest;
 import com.bruno.dao.TaskDao;
+import com.bruno.model.Task;
+import com.bruno.model.TaskHibernate;
 import org.apache.wicket.util.tester.WicketTester;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -29,5 +32,19 @@ public class ListTasksPageTest {
 
         tester.assertVisible("noTasksLabel");
         tester.assertInvisible("taskContainer");
+    }
+
+
+    @Test
+    public void pageWithTasksTest() {
+        Task mockTest = new TaskHibernate(null, "Comprar pão", false);
+
+        when(taskDaoMock.listAllTasks()).thenReturn(List.of(mockTest));
+
+        tester.startPage(ListTasksPage.class);
+
+        tester.assertInvisible("noTasksLabel");
+        tester.assertVisible("taskContainer");
+        tester.assertLabel("taskContainer:tasks:0:title", "Comprar pão");
     }
 }
